020f85d87b3322bd96d0427dad217e273c186d1d
Karuppiah Natarajan <karuppiah7890@gmail.com>
2020-12-31 15:32:52 +0530

    always run command with bash

--- pkg/cmd.go
+++ pkg/cmd.go
@@ -3,11 +3,12 @@ package pkg
 import (
 	"fmt"
 	"os/exec"
+	"strings"
 )
 
 // Something does something
 func Something(scriptPath string) {
-	cmd := exec.Command(scriptPath, "wow", "cool!")
+	cmd := exec.Command("bash", "-c", strings.Join([]string{scriptPath, "wow", "cool!"}, " "))
 	output, err := cmd.CombinedOutput()
 	if err != nil {
 		panic(err)
