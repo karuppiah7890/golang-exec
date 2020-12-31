d17ec4387522fb74f1e09c7b062e646415b29c23
Karuppiah Natarajan <karuppiah7890@gmail.com>
2020-12-31 13:35:14 +0530

    use exe file when running test on windows

--- pkg/cmd_test.go
+++ pkg/cmd_test.go
@@ -2,9 +2,15 @@ package pkg
 
 import (
 	"path/filepath"
+	"runtime"
 	"testing"
 )
 
 func TestSomething(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		Something(filepath.Join("..", "test", "script.exe"))
+		return
+	}
+
 	Something(filepath.Join("..", "test", "script"))
 }
